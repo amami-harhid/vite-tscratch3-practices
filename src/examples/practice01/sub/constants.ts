5da58f72d9096c1be357f1bac0d55bdb2d94cf1b
amami.thidamall.0001 <amami.thidamall.0001@gmail.com>
2025-07-20 00:16:27 +0000

    update

--- src/examples/practice01/sub/constants.ts
+++ src/examples/practice01/sub/constants.ts
@@ -1,4 +1,9 @@
+/**
+ * コンスタント定義 2025/07/20 09:14
+ */
 export const Constants = {
+    /** ネコ画像ID */
     //CAT: 'CAT',
-    APPLE: 'APPLE', // ★ 変更
+    /** リンゴ画像ID */
+    APPLE: 'APPLE', // ★ ⇒変更
 }
--- src/practices/practice01/sub/constants.ts
+++ src/practices/practice01/sub/constants.ts
@@ -1,3 +1,7 @@
+/**
+ * コンスタント定義 2025/07/20 09:14
+ */
 export const Constants = {
+    /** ネコ画像ID */
     CAT: 'CAT',
 }
